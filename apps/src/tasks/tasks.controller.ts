514a0c17d3897caecbf63b4b58a80cc429818901
Silence Dogood <lamphuocbao98@gmail.com>
2021-01-19 07:30:19 +0700

    add delete, update api

--- apps/src/tasks/tasks.controller.ts
+++ apps/src/tasks/tasks.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateTaskRequestDto } from './dtos/create-task-request.dto';
-import { Task } from './models/task.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { Task, TaskStatus } from './models/task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -12,12 +23,31 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   // we can extract specific value in body by using @Body('key'). Ex: @Body('title')
-  createTask(@Body() createTaskRequestDto: CreateTaskRequestDto): Task {
-    return this.tasksService.createTask(
-      createTaskRequestDto.title,
-      createTaskRequestDto.description,
-    );
+  createTask(@Body() createTaskRequestDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskRequestDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskById(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatusById(id, status);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string) {
+    try {
+      this.tasksService.deleteTaskById(id);
+    } catch {
+      throw new HttpException('try again', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
--- apps/src/tasks/tasks.service.ts
+++ apps/src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Utils } from 'src/lib/utils';
+import { CreateTaskDto } from './dtos/create-task.dto';
 import { ITask, Task, TaskStatus } from './models/task.model';
 
 @Injectable()
@@ -10,16 +11,32 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  updateTaskStatusById(id: string, newStatus: TaskStatus): Task {
+    const task: Task = this.getTaskById(id);
+    task.status = newStatus;
+
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: ITask = {
       id: Utils.createUUID(),
-      title: title,
-      description: description,
+      title,
+      description,
       status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
 
     return task;
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }

66e3da31677e1f23da6d825521138670d0ce20de
Silence Dogood <lamphuocbao98@gmail.com>
2021-02-06 20:35:43 +0700

    setup docker-compose for postgresql

--- apps/src/config/database-config-info.ts
+++ apps/src/config/database-config-info.ts
@@ -4,7 +4,7 @@ export const DatabaseConfigInfo = new Map<string, string>([
   [DATABASE_CONFIG_FIELD.type, 'postgres'],
   [DATABASE_CONFIG_FIELD.host, 'localhost'],
   [DATABASE_CONFIG_FIELD.port, '5432'],
-  [DATABASE_CONFIG_FIELD.username, 'postgres'],
-  [DATABASE_CONFIG_FIELD.password, '@Naruto4ever'],
+  [DATABASE_CONFIG_FIELD.username, 'baobao'],
+  [DATABASE_CONFIG_FIELD.password, 'pelongla'],
   [DATABASE_CONFIG_FIELD.database, 'taskmanagement'],
 ]);
